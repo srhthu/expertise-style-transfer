fb6d909a75f3eeee8ad69b069d995a3f7b8a3de4
Ray GG <shuirh2019@gmail.com>
2020-05-09 17:50:25 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,70 @@
 document.addEventListener("DOMContentLoaded", main);
 
 function main(){
+    $("#btn-verify").click(verify);
+    $("#btn-show").click(show_another);
+    $(".interact-sent").click(example_click);
+    show_another();
     draw_medical_topic();
     draw_PCIO();
+}
+
+var example_data = [
+    [
+        ["e", "Pain is usually felt over the inflamed site, but referred pain is possible."], ["ne", "The pain is usually felt in the chest wall right over the site of the inflammation or infection that caused the effusion."]],
+    [
+        ["ne", "Symptoms of granuloma inguinale usually begin 1 to 12 weeks after infection. The first symptom is a painless, red nodule that slowly enlarges into a round, raised, foul-smelling lump."], 
+        ["e", "After an incubation period of about 1 to 12 weeks, a painless, red skin nodule slowly enlarges, becoming a raised, beefy red, moist, smooth, foul-smelling lesion."]],
+    [
+        ["e", "The lesion slowly enlarges, often ulcerates, and may spread to other skin areas. Lesions heal slowly, with scarring."],
+        ["ne", "The sores slowly enlarge and spread to nearby tissue, causing further damage. Sores heal slowly and may result in permanent scar."]
+    ],
+    [
+        ["e", "The patients usually feel painful when affected tendons are actively moved or their natural motions are resisted."],
+        ["ne", "People always feel painful when the inflamed tendons are moved or pressed."],
+    ],
+    [
+        ["e", "Cardiac complications include pericarditis (most commonly)，myocarditis，endocardium and coronary artery vasculitis."],
+        ["ne", "People with lupus may have chest pain due to inflammation of the sac around the heart (pericarditis). More serious but rare effects on the heart are inflammation of the walls of the coronary arteries (coronary artery vasculitis), which can lead to angina; the inflammation of  the surfaces of the heart valve to thicken or develop lesions(bacterial endocarditis); and inflammation of the heart muscle (myocarditis), which can lead to heart failure."],
+    ]
+];
+var example_idx = -1;
+
+function verify(){
+    verify_one($("#int-sent-1"));
+    verify_one($("#int-sent-2"));
+}
+
+function verify_one(sent_div){
+    var status = sent_div.attr("data-status");
+    var label = sent_div.attr("data-label");
+    console.log(status);
+    console.log(label);
+    if(status=="select" && label == "e"){
+        sent_div.attr("data-status", "right");
+    }
+    else if(status=="select" && label == "ne"){
+        sent_div.attr("data-status", "wrong");
+    }
+}
+
+function show_another(){
+    example_idx = example_idx+1<example_data.length?example_idx+1:0;
+    $(".interact-sent").attr("data-status", "normal");
+    $("#int-sent-1 p").text(example_data[example_idx][0][1]);
+    $("#int-sent-1").attr("data-label", example_data[example_idx][0][0]);
+    $("#int-sent-2 p").text(example_data[example_idx][1][1]);
+    $("#int-sent-2").attr("data-label", example_data[example_idx][1][0]);
+}
+
+function example_click(){
+    if($(this).attr("data-status")=="normal"){
+        $(this).attr("data-status", "select");
+        $(this).siblings().attr("data-status", "normal");
+    }
+    else if($(this).attr("data-status")=="select"){
+        $(this).attr("data-status", "normal");
+    }
 }
 
 function draw_medical_topic(){
